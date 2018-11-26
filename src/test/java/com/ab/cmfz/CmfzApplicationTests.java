64b7268a0e9b780fa1f2531b306d4c4e158d922d
gzl <wgguozhilong@163.cpm>
2018-11-26 23:09:57 +0800

    init project

--- src/test/java/com/ab/cmfz/CmfzApplicationTests.java
+++ src/test/java/com/ab/cmfz/CmfzApplicationTests.java
@@ -1,17 +1,24 @@
 package com.ab.cmfz;
 
+import com.ab.cmfz.dao.UserDao;
+import com.ab.cmfz.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CmfzApplicationTests {
+public class CmfzApplicationTests extends TmallApplicationTests {
 
+    @Autowired
+    UserDao userDao;
     @Test
     public void contextLoads() {
-        System.out.println(15);
+        User user = userDao.selectByPrimaryKey(12);
+        System.out.println(user);
+
     }
 
 }
