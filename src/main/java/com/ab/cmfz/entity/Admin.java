954c78b41c3ab61096e93c1e11255203ae752a72
gzl <wgguozhilong@163.cpm>
2018-11-27 13:01:48 +0800

    init project

---
+++ src/main/java/com/ab/cmfz/entity/Admin.java
@@ -0,0 +1,14 @@
+package com.ab.cmfz.entity;
+
+import lombok.Data;
+
+@Data
+public class Admin {
+    private Integer id;
+
+    private String name;
+
+    private String password;
+
+
+}
---
+++ src/main/java/com/ab/cmfz/entity/Menu.java
@@ -0,0 +1,18 @@
+package com.ab.cmfz.entity;
+
+import lombok.Data;
+
+@Data
+public class Menu {
+    private Integer id;
+
+    private String title;
+
+    private String iconcls;
+
+    private Integer parentId;
+
+    private String url;
+
+
+}
