a318b234de235a0706467c64518ea0bd5f6fc248
gzl <wgguozhilong@163.cpm>
2018-11-27 13:16:38 +0800

    init project

--- src/main/java/com/ab/cmfz/entity/User.java
+++ src/main/java/com/ab/cmfz/entity/User.java
@@ -1,7 +1,10 @@
 package com.ab.cmfz.entity;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class User {
     private Integer id;
 
@@ -29,107 +32,4 @@ public class User {
 
     private Date date;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getPhonenum() {
-        return phonenum;
-    }
-
-    public void setPhonenum(String phonenum) {
-        this.phonenum = phonenum == null ? null : phonenum.trim();
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username == null ? null : username.trim();
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password == null ? null : password.trim();
-    }
-
-    public String getSalt() {
-        return salt;
-    }
-
-    public void setSalt(String salt) {
-        this.salt = salt == null ? null : salt.trim();
-    }
-
-    public String getDharmaname() {
-        return dharmaname;
-    }
-
-    public void setDharmaname(String dharmaname) {
-        this.dharmaname = dharmaname == null ? null : dharmaname.trim();
-    }
-
-    public String getProvince() {
-        return province;
-    }
-
-    public void setProvince(String province) {
-        this.province = province == null ? null : province.trim();
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city == null ? null : city.trim();
-    }
-
-    public Integer getSex() {
-        return sex;
-    }
-
-    public void setSex(Integer sex) {
-        this.sex = sex;
-    }
-
-    public String getSign() {
-        return sign;
-    }
-
-    public void setSign(String sign) {
-        this.sign = sign == null ? null : sign.trim();
-    }
-
-    public String getHeadpic() {
-        return headpic;
-    }
-
-    public void setHeadpic(String headpic) {
-        this.headpic = headpic == null ? null : headpic.trim();
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
 }
