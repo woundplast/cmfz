9b57c2a686450fcce2b669af8859051f81fe51b0
gzl <wgguozhilong@163.cpm>
2018-11-27 13:24:05 +0800

    init project

--- src/main/java/com/ab/cmfz/entity/Admin.java
+++ src/main/java/com/ab/cmfz/entity/Admin.java
@@ -1,5 +1,8 @@
 package com.ab.cmfz.entity;
 
+import lombok.Data;
+
+@Data
 public class Admin {
     private Integer id;
 
@@ -7,27 +10,4 @@ public class Admin {
 
     private String password;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name == null ? null : name.trim();
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password == null ? null : password.trim();
-    }
 }
