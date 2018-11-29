281be9cb1746a11b4bae7aeeadab37ab8808a5bf
gzl <wgguozhilong@163.cpm>
2018-11-29 14:07:12 +0800

    init project

---
+++ src/main/java/com/ab/cmfz/service/Impl/AlbumServiceImpl.java
@@ -0,0 +1,10 @@
+package com.ab.cmfz.service.Impl;
+
+import com.ab.cmfz.service.AlbumService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("albumService")
+@Transactional
+public class AlbumServiceImpl implements AlbumService {
+}
