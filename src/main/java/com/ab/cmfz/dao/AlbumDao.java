daf34bf2fb8361df29767ec6faa9e4edf365f619
gzl <wgguozhilong@163.cpm>
2018-12-02 18:57:49 +0800

    init project

--- src/main/java/com/ab/cmfz/dao/AlbumDao.java
+++ src/main/java/com/ab/cmfz/dao/AlbumDao.java
@@ -6,17 +6,10 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 public interface AlbumDao {
-    int deleteByPrimaryKey(Integer id);
 
-    int insert(Album record);
-
-    int insertSelective(Album record);
 
     Album selectByPrimaryKey(Integer id);
 
-    int updateByPrimaryKeySelective(Album record);
-
-    int updateByPrimaryKey(Album record);
 
     List<Album> queryAllAlbum(@Param("start") int start, @Param("pageSize") int pageSize);
 
